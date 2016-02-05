fe9ec25ec2ed2a2680170ebd59134fbe8bd55117
David G. Paul <jedi58@gmail.com>
2016-02-05 19:06:50 +0000

    Improved code quality of `Worklog` object

--- src/Worklog.php
+++ src/Worklog.php
@@ -9,33 +9,33 @@ use Inachis\Component\JiraIntegration\JiraConnection;
  * resource from the Jira API
  */
 class Worklog extends JiraConnection {
-	/**
-	 * @var Authentication Reference to instance of self
-	 */
-	private static $instance;
-	/**
-	 * Returns a singleton instance of this class
-	 * @return Worklog The singleton instance
-	 */
-	public static function getInstance()
-	{
-		if (null === static::$instance) {
-			static::$instance = new static();
-		}
-		return static::$instance;
-	}
+    /**
+     * @var Authentication Reference to instance of self
+     */
+    private static $instance;
+    /**
+     * Returns a singleton instance of this class
+     * @return Worklog The singleton instance
+     */
+    public static function getInstance()
+    {
+        if (null === static::$instance) {
+            static::$instance = new static();
+        }
+        return static::$instance;
+    }
     /**
      * 
-     * @param string $issue_key The issue to add the worklog update to
+     * @param string $issueKey The issue to add the worklog update to
      * @param string|int $timeSpent The amount of time spent on the current issue
      * @param string[] $options Any additional options that should be applied
      * @return stdClass The result of adding the worklog entry
      */
-    public function create($issue_key, $timeSpent = '', $options = array())
+    public function create($issueKey, $timeSpent = '', $options = array())
     {
         $timeKey = is_int($timeSpent) ? 'timeSpentSeconds' : 'timeSpent';
         return $this->sendRequest(
-            'issue/' . urlencode($issue_key) . '/worklog',
+            'issue/' . urlencode($issueKey) . '/worklog',
             array_merge(
                 array(
                     $timeKey => $timeSpent
@@ -47,22 +47,22 @@ class Worklog extends JiraConnection {
     }
     /**
      * Updates an existing worklog entry
-     * @param string $issue_key The issue to add the worklog update to
-     * @param string $worklog_key The ID of the worklog entry to update
+     * @param string $issueKey The issue to add the worklog update to
+     * @param string $worklogKey The ID of the worklog entry to update
      * @param string|int $timeSpent The amount of time spent on the current issue
      * @param string[] $options Any additional options that should be applied
      * @return stdClass The result of updating the worklog entry
      */
     public function update(
-            $issue_key,
-            $worklog_key,
-            $timeSpent = '',
-            $options = array()
+        $issueKey,
+        $worklogKey,
+        $timeSpent = '',
+        $options = array()
     ) {
         $timeKey = is_int($timeSpent) ? 'timeSpentSeconds' : 'timeSpent';
         return $this->sendRequest(
-            'issue/' . urlencode($issue_key) . 
-                '/worklog/' . urlencode($worklog_key),
+            'issue/' . urlencode($issueKey) . 
+                '/worklog/' . urlencode($worklogKey),
             array_merge(
                 array(
                     $timeKey => $timeSpent
@@ -74,40 +74,40 @@ class Worklog extends JiraConnection {
     }
     /**
      * Deletes a specific worklog entry
-     * @param string $issue_key The issue to add the worklog update to
-     * @param string $worklog_key The ID of the worklog entry to update
+     * @param string $issueKey The issue to add the worklog update to
+     * @param string $worklogKey The ID of the worklog entry to update
      * @return stdClass The result of updating the worklog entry
      */
-    public function delete($issue_key, $worklog_key)
+    public function delete($issueKey, $worklogKey)
     {
         return $this->sendRequest(
-            'issue/' . urlencode($issue_key) . 
-                '/worklog/' . urlencode($worklog_key),
+            'issue/' . urlencode($issueKey) . 
+                '/worklog/' . urlencode($worklogKey),
             array(),
             'DELETE'
         );
     }
     /**
      * Returns a specific worklog entry for a given Issue
-     * @param string $issue_key The issue to add the worklog update to
+     * @param string $issueKey The issue to add the worklog update to
      * @param string $worklog_key The ID of the worklog entry to update
      * @return stdClass The result of updating the worklog entry
      */
-    public function get($issue_key, $worklog_key)
+    public function get($issueKey, $worklogKey)
     {
         return $this->sendRequest(
-            'issue/' . urlencode($issue_key) . 
-                '/worklog/' . urlencode($worklog_key)
+            'issue/' . urlencode($issueKey) . 
+                '/worklog/' . urlencode($worklogKey)
         );
     }
     /**
      * Returns all worklog entries for a given issue
-     * @param string $issue_key The issue to add the worklog update to
+     * @param string $issueKey The issue to add the worklog update to
      * @return stdClass The result of updating the worklog entry
      */
-    public function getAll($issue_key)
+    public function getAll($issueKey)
     {
-        return $this->sendRequest('issue/' . urlencode($issue_key) . 
+        return $this->sendRequest('issue/' . urlencode($issueKey) . 
                 '/worklog/');
     }
 }
