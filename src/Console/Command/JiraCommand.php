0447a23f4a59ffdf52e12c9e1847c6cf122ee26e
David G. Paul <jedi58@gmail.com>
2016-02-11 14:04:24 +0000

    [FEATURE] Added support for YAML based config files.
    This will allow you to set defaults for the Jira API URL, the base64 encoded username:password pair. In an upcoming release it will also support the supplying of a default project to use.

--- src/Console/Command/JiraCommand.php
+++ src/Console/Command/JiraCommand.php
@@ -6,6 +6,7 @@ use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Yaml\Yaml;
 use Inachis\Component\JiraIntegration\Authentication;
 
 /**
@@ -13,15 +14,27 @@ use Inachis\Component\JiraIntegration\Authentication;
  */
 abstract class JiraCommand extends Command
 {
+    const CONFIG_FILE = '/../../../jira.yml';
     protected $auth;
 
     protected $helper;
 
     protected function configure()
     {
+        $authProvided = false;
+        // get config from YAML if available
+        if ($this->canAccessYamlConfig()) {
+            $config = Yaml::parse(file_get_contents(__DIR__ . self::CONFIG_FILE));
+            $this->setAuthentication(
+                $config['default']['url'],
+                $config['default']['auth']
+            );
+            $authProvided = true;
+        }
         $this->addOption(
             'auth',
             null,
+            $authProvided ? InputOption::VALUE_OPTIONAL :
             InputOption::VALUE_REQUIRED,
             'The base64 encoded username:password pair to use for Jira API 
                 authentication'
@@ -29,6 +42,7 @@ abstract class JiraCommand extends Command
         $this->addOption(
             'url',
             null,
+            $authProvided ? InputOption::VALUE_OPTIONAL :
             InputOption::VALUE_REQUIRED,
             'The URL of the Jira API to connect to'
         );
@@ -36,13 +50,26 @@ abstract class JiraCommand extends Command
 
     protected function connect($url, $credentials)
     {
-        if (empty($url)) {
+        if (!empty($url) && !empty($credentials)) {
+            $this->setAuthentication($url, $credentials);
+        }
+        if (empty(Authentication::getInstance()->getApiBaseUrl())) {
             throw new \InvalidArgumentException('Jira API URL must be provided');
         }
-        if (empty($credentials)) {
+        if (empty(Authentication::getInstance()->getApiAuth())) {
             throw new \InvalidArgumentException('Credentials must be provided');
         }
+    }
+
+    private function canAccessYamlConfig()
+    {
+        return file_exists(__DIR__ . self::CONFIG_FILE) &&
+            is_readable(__DIR__ . self::CONFIG_FILE);
+    }
+
+    private function setAuthentication($url, $auth)
+    {
         $this->auth = Authentication::getInstance($url);
-        $this->auth->setApiAuth($credentials);
+        $this->auth->setApiAuth($auth);
     }
 }
