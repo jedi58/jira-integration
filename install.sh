4b62449fd24625b779137719d467253dab53dd4c
Dave Paul <dave.paul@jadu.net>
2016-04-07 15:12:54 +0100

    Updated permissions

--- install.sh
+++ install.sh
@@ -3,8 +3,5 @@ COMPOSER_IS_INSTALLED=$?
 if [[ $COMPOSER_IS_INSTALLED -ne 0 ]]; then
   curl -sS https://getcomposer.org/installer | php
 fi
-
 php composer.phar install
-chmod +x app/console
-
 ./app/console --help
