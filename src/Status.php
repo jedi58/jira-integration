51200f607ec486095db2ab40e0ac6622fc147ff2
David G. Paul <jedi58@gmail.com>
2017-05-11 10:37:55 +0100

    [ADDED] `Status` object added This will return an object containing all statuses within Jira

---
+++ src/Status.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Inachis\Component\JiraIntegration;
+
+use Inachis\Component\JiraIntegration\JiraConnection;
+
+/**
+ * Object for interacting with Jira API statuses
+ */
+class Status extends JiraConnection
+{
+    /**
+     * @var Authentication Reference to instance of self
+     */
+    private static $instance;
+    /**
+     * Returns a singleton instance of this class
+     * @return User The singleton instance
+     */
+    public static function getInstance()
+    {
+        if (null === static::$instance) {
+            static::$instance = new static();
+        }
+        return static::$instance;
+    }
+    /**
+     * Returns all statuses from Jira
+     * @return stdClass The object containing all statuses
+     */
+    public function get()
+    {
+        return $this->sendRequest('status');
+    }
+}
